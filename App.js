6966a10c6234f26074563c851d77f70b596797db
fred <fredkeemhaus@gmail.com>
2019-01-20 16:33:19 +0900

    Coding the UI part Three

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default class App extends React.Component {
             returnKeyType={"done"}
             autoCorrect={false}
           />
-          <ScrollView>
+          <ScrollView contentContainerStyle={styles.toDos}>
             <ToDo />
           </ScrollView>
         </View>
@@ -86,5 +86,8 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth,
     fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
